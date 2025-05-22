8caac57ad948da55f84bdbf54889f35d7e44fa7a
shnhlms <nedalturas527@gmail.com>
2025-05-22 14:34:39 +0000

    created an active state styling in navigation bar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function Navbar() {
+
+  const location = useLocation()
+
+  const isActive = (path) => location.pathname === path
 
   return (
     <div className="ui massive borderless menu" style={{ backgroundColor: 'var(--company-color)', position: 'sticky', top: '0', fontWeight: 'bolder', zIndex: 100, boxShadow: '1px 5px 10px -6px rgba(71,71,71,1)' }}>
@@ -11,8 +15,15 @@ function Navbar() {
         </div>
 
         <div className="right menu">
-          <Link to="/" className="item">Database</Link>
-          <Link to="/faq" className="item">FAQ</Link>
+          <Link to="/" className="item" style={{ 
+              backgroundColor: isActive('/') ? 'rgba(255,255,255,0.1)' : 'transparent',
+              borderBottom: isActive('/') ? '2px solid white' : 'none'
+            }}>Database</Link>
+          <Link to="/faq" className="item" style={{ 
+              backgroundColor: isActive('/faq') ? 'rgba(255,255,255,0.1)' : 'transparent',
+              borderBottom: isActive('/faq') ? '2px solid white' : 'none'
+            }}
+>FAQ</Link>
         </div>
 
       </div>
